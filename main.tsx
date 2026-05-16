4dabced27144261ebe87a4421d194346bf038d58
Emviio <rushnaishtiaq6@gmail.com>
2026-05-16 17:04:16 +0500

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,38 @@
+import __vite__cjsImport0_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=be9817fe"; const createRoot = __vite__cjsImport0_reactDom_client["createRoot"];
+import "/src/index.css";
+import App from "/src/App.tsx";
+import { AppWrapper } from "/src/components/common/PageMeta.tsx";
+var _jsxFileName = "/workspace/app-bmwqgqfmfcox/src/main.tsx";
+import __vite__cjsImport4_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=428b684f"; const _jsxDEV = __vite__cjsImport4_react_jsxDevRuntime["jsxDEV"];
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ _jsxDEV(AppWrapper, {
+	"data-md-id": "src/main.tsx:7:2",
+	"data-md-name": "AppWrapper",
+	"data-component-is-root": "true",
+	"data-component-path": "src/main.tsx",
+	"data-component-line": "7",
+	"data-component-end-line": "9",
+	"data-component-file": "main.tsx",
+	"data-component-name": "AppWrapper",
+	"data-component-content": "%7B%22className%22%3A%22%22%2C%22allowLink%22%3Atrue%7D",
+	children: /* @__PURE__ */ _jsxDEV(App, {
+		"data-md-id": "src/main.tsx:8:4",
+		"data-md-name": "App",
+		"data-component-is-root": "false",
+		"data-component-path": "src/main.tsx",
+		"data-component-line": "8",
+		"data-component-end-line": "8",
+		"data-component-file": "main.tsx",
+		"data-component-name": "App",
+		"data-component-content": "%7B%22className%22%3A%22%22%2C%22allowLink%22%3Atrue%7D"
+	}, void 0, false, {
+		fileName: _jsxFileName,
+		lineNumber: 8,
+		columnNumber: 5
+	}, this)
+}, void 0, false, {
+	fileName: _jsxFileName,
+	lineNumber: 7,
+	columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBQUEsU0FBUyxrQkFBa0I7QUFDM0IsT0FBTztBQUNQLE9BQU8sU0FBUztBQUNoQixTQUFTLGtCQUFrQjs7O0FBRTNCLFdBQVcsU0FBUyxlQUFlLE9BQU8sQ0FBRSxDQUFDLE9BQzNDLHdCQUFDO0NBQVM7Q0FBQTtDQUFBO0NBQUE7Q0FBQTtDQUFBO0NBQUE7Q0FBQTtDQUFBO1dBQ1Isd0JBQUM7RUFBRTtFQUFBO0VBQUE7RUFBQTtFQUFBO0VBQUE7RUFBQTtFQUFBO0VBQUE7Ozs7O1NBQUc7Ozs7O1FBQ0ksQ0FDYiIsIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZXMiOlsibWFpbi50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgY3JlYXRlUm9vdCB9IGZyb20gXCJyZWFjdC1kb20vY2xpZW50XCI7XG5pbXBvcnQgXCIuL2luZGV4LmNzc1wiO1xuaW1wb3J0IEFwcCBmcm9tIFwiLi9BcHAudHN4XCI7XG5pbXBvcnQgeyBBcHBXcmFwcGVyIH0gZnJvbSBcIi4vY29tcG9uZW50cy9jb21tb24vUGFnZU1ldGEudHN4XCI7XG5cbmNyZWF0ZVJvb3QoZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJyb290XCIpISkucmVuZGVyKFxuICA8QXBwV3JhcHBlcj5cbiAgICA8QXBwIC8+XG4gIDwvQXBwV3JhcHBlcj5cbik7XG4iXSwiZmlsZSI6Ii93b3Jrc3BhY2UvYXBwLWJtd3FncWZtZmNveC9zcmMvbWFpbi50c3gifQ==
